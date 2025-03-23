6c50b42a534e49f5befeaccf1b6f3c88f0d57650
Vitor Veiga Vasconcelos Neto <108374481+vitorneto43@users.noreply.github.com>
2025-03-23 13:34:04 -0300

    Update RegisterForm.js

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -10,7 +10,7 @@ function RegisterForm() {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:5000/register', {
+      const res = await fetch('https://game-hive-1.onrender.com/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, email, password })
